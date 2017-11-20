a43e6d8720e463400e9afdf2dea06bf5cc66bbe4
Radu Dan <radu@full-throttle.ro>
2017-11-20 02:31:38 +0100

    Lazy load thumbnail images

--- Makefile
+++ Makefile
@@ -1,12 +1,21 @@
 default:
-	pipenv run flask run --host=0.0.0.0 --port=5001 &
+	make server &
+	make client
+
+client:
 	node_modules/.bin/gulp watch
+
+server:
+	pipenv run flask run --host=0.0.0.0 --port=5001
+
 thumbs:
 	pipenv run python update_thumbs.py
 	mogrify -fuzz 10% -trim +repage static/thumbs/*
+
 deps:
 	npm install
 	pipenv update
+
 production:
 	git fetch
 	git reset --hard origin/master
--- src/javascripts/modules/menu-control.js
+++ src/javascripts/modules/menu-control.js
@@ -7,5 +7,25 @@ export default function() {
         event.preventDefault();
         control.classList.toggle(CLASS_ACTIVE);
         menu.classList.toggle(CLASS_ACTIVE);
+
+        if(control.classList.contains(CLASS_ACTIVE)) {
+            trigger(12);
+        }
     });
+
+
+    // delayed image loading; call trigger(n) to speed it up
+    const images = document.querySelectorAll('[data-delay]');
+    const load = function() { this.src = this.dataset.src; }
+    const triggers = Array.from(images).map((image) => {
+        const trigger = load.bind(image);
+        setTimeout(trigger, image.dataset.delay);
+        return trigger;
+    });
+
+    function trigger(n) {
+        for(let i=0; i<n; i++)
+            if(typeof triggers[i] !== 'undefined')
+                triggers[i]();
+    }
 };
