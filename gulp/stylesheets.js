9abccb47d66341b65d46da4bbb5ac69a0b3e9816
Radu Dan <radu@full-throttle.ro>
2017-11-21 03:35:26 +0100

    Add sass sourcemaps

--- gulp/stylesheets.js
+++ gulp/stylesheets.js
@@ -4,21 +4,24 @@ const del = require('del');
 const gulp = require('gulp');
 const paths = require('./paths');
 const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 
 module.exports = {
-  clean: () => {
+  clean() {
     del(`${paths.dest.stylesheets}`, {force: true});
   },
-  build: () => {
+  build() {
     const postCSSPlugins = [
       autoprefixer({browsers: ['last 2 versions']}),
       cssnano()
     ];
 
     return gulp.src(`${paths.source.stylesheets}/**/*.scss`)
+      .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
       .pipe(postcss(postCSSPlugins))
+      .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest(paths.dest.stylesheets));
   }
 };
